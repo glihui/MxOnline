f0ba7b2113648a8e5b2851266d800a89f03f7207
guolihui <15919476778@163.com>
2018-09-19 18:15:40 +0800

    8-6 13:57

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -4,12 +4,19 @@ __date__ = '18-9-18 下午3:28'
 
 from django.urls import path, include, re_path
 
-from .views import CourseListView, CourseDetailView
+from .views import CourseListView, CourseDetailView, CourseInfoView, CommentsView
 
 urlpatterns = [
     # 课程列表页
     path('list/', CourseListView.as_view(), name="course_list"),
     re_path('detail/(?P<course_id>.*)/', CourseDetailView.as_view(), name="course_detail"),
+    re_path('info/(?P<course_id>.*)/', CourseInfoView.as_view(), name="course_info"),
+
+    # 课程评论
+    re_path('comment/(?P<course_id>.*)/', CommentsView.as_view(), name="course_comments"),
+
+    # 添加课程评论
+    # re_path('comment/(?P<course_id>.*)/', CommentsView.as_view(), name="course_comments"),
 ]
 
 app_name = 'course'
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 
-from .models import Course
+from .models import Course, CourseResource
+from operation.models import UserFavorite, CourseComments
 
 # Create your views here.
 
@@ -20,7 +22,6 @@ class CourseListView(View):
                 all_courses = all_courses.order_by("-students")
             elif sort == 'hot':
                 all_courses = all_courses.order_by("-click_nums")
-
 
         # 对课程进行分页
         try:
@@ -50,6 +51,71 @@ class CourseDetailView(View):
         course.click_nums += 1
         course.save()
 
+        has_fav_course = False
+        has_fav_org = False
+
+        if request.user.is_authenticated:
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
+
+        tag = course.tag
+        if tag:
+            relate_courses = Course.objects.filter(tag=tag)[:1]
+        else:
+            relate_courses = []
         return render(request, "course-detail.html", {
-            "course": course
+            "course": course,
+            "relate_courses": relate_courses,
+            "has_fav_course": has_fav_course,
+            "has_fav_org": has_fav_org
         })
+
+
+class CourseInfoView(View):
+    """
+    课程章节信息
+    """
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        return render(request, "course-video.html", {
+            "course": course,
+            "course_resources": all_resources
+        })
+
+
+class CommentsView(View):
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.all()
+        return render(request, "course-comment.html", {
+            "course": course,
+            "course_resources": all_resources,
+            "all_comments": all_comments
+        })
+
+
+class AddComentsView(View):
+    """
+    用户添加课程评论
+    """
+    def post(self, request):
+        if not request.user.is_authenticated:
+            # 判断用户登录状态
+            return HttpResponse('{"status": "fail", "msg":"用户未登录"}', content_type='application/json')
+
+        course_id = request.POST.get("couse_id", 0)
+        comments = request.POST.get("comments", "")
+        if course_id > 0 and comments:
+            course_comments = CourseComments()
+            course = Course.objects.get(id=int(course_id))
+            course_comments.course = course
+            course_comments.comments = comments
+            course_comments.user = request.user
+            course_comments.save()
+            return HttpResponse('{"status": "success", "msg":"添加成功"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status": "fail", "msg":"添加失败"}', content_type='application/json')
