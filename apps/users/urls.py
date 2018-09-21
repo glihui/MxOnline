9b2c073e8261a97349efb5f0a17ae874363539e5
guolihui <15919476778@163.com>
2018-09-21 17:56:48 +0800

    10-6

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,7 +4,7 @@ __date__ = '18-9-21 下午2:56'
 
 from django.urls import path, include, re_path
 
-from .views import UserinfoView, UploadImageView
+from .views import UserinfoView, UploadImageView, UpdatePwdView
 
 urlpatterns = [
     # 用户信息
@@ -12,6 +12,9 @@ urlpatterns = [
 
     # 用户头像上传
     path('image/upload/', UploadImageView.as_view(), name="image_upload"),
+
+    # 用户个人中心修改密码
+    path('update/pwd/', UpdatePwdView.as_view(), name="update_pwd"),
 ]
 
 app_name = 'users'
