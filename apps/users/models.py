067bdcaa556e47655f3f5ed70fb2da7be9dee8ab
郭黎辉 <guolihui@guolihuideMacBook-Pro.local>
2018-09-23 00:07:22 +0800

    11-1

--- apps/users/models.py
+++ apps/users/models.py
@@ -25,11 +25,19 @@ class UserProfile(AbstractUser):
     def __str__(self):
         return self.username
 
+    def unread_nums(self):
+        # 获取用户未读消息的数量
+        from operation.models import UserMessage
+        return UserMessage.objects.filter(user=self.id).count()
+
 
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name=u"验证码")
     email = models.EmailField(max_length=50, verbose_name=u"邮箱")
-    send_type = models.CharField(verbose_name=u"验证码类型", choices=(("register", u"注册"), ("forget", u"找回密码")), max_length=10)
+    send_type = models.CharField(verbose_name=u"验证码类型", choices=(
+        ("register", u"注册"),
+        ("forget", u"找回密码"),
+        ("update_email", u"修改邮箱")), max_length=20)
     send_time = models.DateTimeField(verbose_name=u"发送时间", default=datetime.now)
 
     class Meta:
@@ -38,6 +46,7 @@ class EmailVerifyRecord(models.Model):
 
     def __str__(self):
         return '{0}({1})'.format(self.code, self.email)
+
 
 class Banner(models.Model):
     title = models.CharField(max_length=100, verbose_name=u"标题")
